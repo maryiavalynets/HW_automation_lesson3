25a44c1cf971060c220d4ad4c98579d0b085dedc
Maryia Valynets <maryia.valynets@gmail.com>
2022-10-01 13:02:37 -0700

    Change function's names

--- Product_page_steps.py
+++ Product_page_steps.py
@@ -3,21 +3,21 @@ from behave import given, when, then
 
 
 @when('Click on the quantity icon')
-def click_on_quantity_icon(context):
+def select_quantity(context):
   context.driver.find_element(By.ID, 'a-autoid-0-announce').click()
 
 
 @when('Click on the number "2" from the quantity values')
-def click_on_quantity_icon(context):
+def select_quantity(context):
   context.driver.find_element(By.ID, 'quantity_1').click()
 
 
 @when('Click on the "Add to Cart" button')
-def click_on_quantity_icon(context):
+def select_quantity(context):
   context.driver.find_element(By.ID, 'add-to-cart-button').click()
 
 
 @when('Click on the "Cart" button')
-def click_on_cart_button(context):
+def click_on_add_to_cart(context):
   cart_button = context.driver.find_element(By.ID,'attach-view-cart-button-form')
   cart_button.click()
